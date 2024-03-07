cdfe6ef3ca12aa1237b6efc5cd26141824d76734
Brayden <coghill30@gmail.com>
2024-03-06 23:40:27 -0800

    Fixed issue with flagging and unflagging mines

--- src/main/java/org/example/mine_sweeper/MineSweeper.java
+++ src/main/java/org/example/mine_sweeper/MineSweeper.java
@@ -12,97 +12,113 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class MineSweeper extends Application {
+import java.util.Random;
 
+public class MineSweeper extends Application {
     private int revealedSafeCells = 0;
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    @Override
     public void start(Stage theStage) {
 
-        MineButton[][] buttons = new MineButton[5][5];
-        FaceButton face = new FaceButton();
-        GridPane gp = new GridPane();
         BorderPane bp = new BorderPane();
+        GridPane gp = new GridPane();
         HBox hb = new HBox();
+        MineButton[][] buttons = new MineButton[8][8];
+        FaceButton face = new FaceButton();
+        Random rand = new Random();
 
         hb.getChildren().add(face);
         hb.setAlignment(Pos.TOP_CENTER);
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        face.setOnMouseClicked(e -> {
+            theStage.close();
+            MineSweeper newGame = new MineSweeper();
+            newGame.start(new Stage());
+        });
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
 
                 buttons[i][j] = new MineButton();
                 MineButton temp = buttons[i][j];
+
                 buttons[i][j].setOnMouseClicked(e -> {
                     buttons[1][1].state = 5;
                     buttons[3][2].state = 5;
                     buttons[3][3].state = 5;
                     MouseButton button = e.getButton();
 
-                    if (temp.isClicked) {
+                    // TODO: change so you can unflag a mine after being flagged
+                    // TODO: create method to make a random board every game with the math to place numbers in the right place
+
+                    if (temp.isClicked && button == MouseButton.PRIMARY) {
                         return;
+                    } else if (temp.isClicked && button == MouseButton.SECONDARY) {
+                        temp.isClicked = false;
                     }
 
-                    if (button == MouseButton.PRIMARY && temp == buttons[0][0] ||
-                            button == MouseButton.PRIMARY && temp == buttons[0][1] ||
-                            button == MouseButton.PRIMARY && temp == buttons[0][2] ||
-                            button == MouseButton.PRIMARY && temp == buttons[1][0] ||
-                            button == MouseButton.PRIMARY && temp == buttons[3][1] ||
-                            button == MouseButton.PRIMARY && temp == buttons[4][1] ||
-                            button == MouseButton.PRIMARY && temp == buttons[2][0] ||
-                            button == MouseButton.PRIMARY && temp == buttons[1][2] ||
-                            button == MouseButton.PRIMARY && temp == buttons[2][4] ||
-                            button == MouseButton.PRIMARY && temp == buttons[3][4] ||
-                            button == MouseButton.PRIMARY && temp == buttons[4][4]) {
-
-                        temp.setGraphic(temp.image1);
+                    if ((button == MouseButton.SECONDARY && temp.state == 0) ||
+                            (button == MouseButton.SECONDARY && temp.state == 5)) {
+                        temp.state = 2;
+                        temp.setGraphic(new ImageView(new Image("File:src/main/java/org/example/mine_sweeper/pictures/flag.png")));
                         temp.isClicked = true;
-                        setState(buttons, face, temp);
+                    } else if (button == MouseButton.SECONDARY && temp.state == 2) {
+                        temp.state = 0;
+                        temp.setGraphic(new ImageView(new Image("File:src/main/java/org/example/mine_sweeper/pictures/cover.png")));
+                        temp.isClicked = false;
+                    } else if (button == MouseButton.PRIMARY) {
+
+                        if (temp == buttons[0][0] || temp == buttons[0][1] || temp == buttons[0][2] ||
+                                temp == buttons[1][0] || temp == buttons[3][1] || temp == buttons[4][1] ||
+                                temp == buttons[2][0] || temp == buttons[1][2] || temp == buttons[2][4] ||
+                                temp == buttons[3][4] || temp == buttons[4][4]) {
+
+                            temp.setGraphic(temp.image1);
+                            temp.isClicked = true;
+                            setState(buttons, face, temp);
 
 
-                    } else if (button == MouseButton.PRIMARY && temp == buttons[2][1] ||
-                            button == MouseButton.PRIMARY && temp == buttons[4][2] ||
-                            button == MouseButton.PRIMARY && temp == buttons[2][3] ||
-                            button == MouseButton.PRIMARY && temp == buttons[4][3]) {
+                        } else if (temp == buttons[2][1] || temp == buttons[4][2] || temp == buttons[2][3] ||
+                                temp == buttons[4][3]) {
 
-                        temp.setGraphic(temp.image2);
-                        temp.isClicked = true;
-                        setState(buttons, face, temp);
+                            temp.setGraphic(temp.image2);
+                            temp.isClicked = true;
+                            setState(buttons, face, temp);
 
-                    } else if (button == MouseButton.PRIMARY && temp == buttons[2][2]) {
+                        } else if (temp == buttons[2][2]) {
 
-                        temp.setGraphic(temp.image3);
-                        temp.isClicked = true;
-                        setState(buttons, face, temp);
+                            temp.setGraphic(temp.image3);
+                            temp.isClicked = true;
+                            setState(buttons, face, temp);
 
-                    } else if (button == MouseButton.PRIMARY && temp == buttons[1][1] ||
-                            button == MouseButton.PRIMARY && temp == buttons[3][2] ||
-                            button == MouseButton.PRIMARY && temp == buttons[3][3]) {
+                        } else if (temp == buttons[1][1] || temp == buttons[3][2] || temp == buttons[3][3]) {
 
-                        temp.state = 4;
-                        temp.isClicked = true;
-                        temp.setGraphic(temp.imageMineRed);
-                        face.setGraphic(face.imageLose);
+                            temp.state = 4;
+                            temp.isClicked = true;
+                            temp.setGraphic(temp.imageMineRed);
+                            face.setGraphic(face.imageLose);
 
-                        for (int a = 0; a < 5; a++) {
-                            for (int b = 0; b < 5; b++) {
-                                if (buttons[a][b].state == 5) {
-                                    buttons[a][b].setGraphic(buttons[a][b].imageMine);
+                            for (int a = 0; a < 8; a++) {
+                                for (int b = 0; b < 8; b++) {
+                                    if (buttons[a][b].state == 5) {
+                                        buttons[a][b].setGraphic(buttons[a][b].imageMine);
+                                    }
                                 }
                             }
-                        }
-                        for (int a = 0; a < 5; a++) {
-                            for (int b = 0; b < 5; b++) {
-                                buttons[a][b].setDisable(true);
+                            for (int a = 0; a < 8; a++) {
+                                for (int b = 0; b < 8; b++) {
+                                    buttons[a][b].isClicked = true;
+                                }
                             }
+                        } else {
+                            temp.setGraphic(temp.image0);
+                            temp.isClicked = true;
+                            setState(buttons, face, temp);
                         }
-                    } else {
-                        temp.setGraphic(temp.image0);
-                        temp.isClicked = true;
-                        setState(buttons, face, temp);
                     }
                 });
                 gp.add(buttons[i][j], i, j);
@@ -110,7 +126,7 @@ public class MineSweeper extends Application {
         }
         bp.setTop(hb);
         bp.setCenter(gp);
-
+        theStage.setTitle("Mine Sweeper");
         theStage.setScene(new Scene(bp));
         theStage.show();
     }
@@ -118,15 +134,16 @@ public class MineSweeper extends Application {
     private void setState(MineButton[][] buttons, FaceButton face, MineButton temp) {
         temp.state = 1;
         revealedSafeCells++;
-        int totalSafeCells = 25 - 3; // 25 cells - 3 mines
+        int totalSafeCells = 64 - 10; // 25 cells - 3 mines
         if (revealedSafeCells == totalSafeCells) {
-            face.setGraphic(face.imageWin);
-            for (int a = 0; a < 5; a++) {
-                for (int b = 0; b < 5; b++) {
+            for (int a = 0; a < 8; a++) {
+                for (int b = 0; b < 8; b++) {
                     if (buttons[a][b].state == 5) {
-                        buttons[a][b].setGraphic(buttons[a][b].imageMine);
+                        buttons[a][b].setGraphic(temp.imageMine = new ImageView(
+                                new Image("file:src/main/java/org/example/mine_sweeper/pictures/mine-grey.png")));
                     }
-                    buttons[a][b].setDisable(true);
+                    face.setGraphic(face.imageWin);
+                    buttons[a][b].isClicked = true;
                 }
             }
         }
@@ -136,8 +153,8 @@ public class MineSweeper extends Application {
 class MineButton extends Button {
     int state; // 0 = covered, 1 = uncovered, 2 = flagged, 3 = misflagged, 4 = mine, 5 = covered mine, 6 = Is Clicked
     boolean isClicked = false;
-    ImageView imageCover, image0, image1, image2, image3, image4, image5, image6, image7, image8, imageMine, imageFlag,
-            imageMisFlagged, imageMineRed;
+    ImageView imageCover, image0, image1, image2, image3, image4, image5, image6, image7, image8, imageMine, imageMineRed,
+            imageFlag, imageMisFlag;
 
     public MineButton() {
         imageCover = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/pictures/cover.png"));
@@ -152,9 +169,8 @@ class MineButton extends Button {
         image8 = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/pictures/8.png"));
 
         imageMine = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/pictures/mine-grey.png"));
-        imageFlag = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/pictures/flag.png"));
-        imageMisFlagged = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/pictures/mine-misflagged.png"));
         imageMineRed = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/pictures/mine-red.png"));
+        imageFlag = new ImageView(new Image("File:src/main/java/org/example/mine_sweeper/pictures/flag.png"));
 
         setGraphic(imageCover);
     }
@@ -187,41 +203,6 @@ class RedDigits extends Button {
         imageRed8 = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/digits/8.png"));
         imageRed9 = new ImageView(new Image("file:src/main/java/org/example/mine_sweeper/digits/9.png"));
 
-        setGraphic(imageRed0);
-    }
-
-    public void setGraphic(int gameScore) {
-        switch (gameScore) {
-            case 0:
-                setGraphic(imageRed0);
-                break;
-            case 1:
-                setGraphic(imageRed1);
-                break;
-            case 2:
-                setGraphic(imageRed2);
-                break;
-            case 3:
-                setGraphic(imageRed3);
-                break;
-            case 4:
-                setGraphic(imageRed4);
-                break;
-            case 5:
-                setGraphic(imageRed5);
-                break;
-            case 6:
-                setGraphic(imageRed6);
-                break;
-            case 7:
-                setGraphic(imageRed7);
-                break;
-            case 8:
-                setGraphic(imageRed8);
-                break;
-            case 9:
-                setGraphic(imageRed9);
-                break;
-        }
+        setGraphic(imageRed9);
     }
 }
